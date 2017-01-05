42624c23aa1b3cf88ece0e0ac26dc5cfa183b10d
Zhou Bowei <zhoubw12@mails.tsinghua.edu.cn>
2017-01-05 15:31:36 +0800

    :dancers: 计算梯度

--- python/main.py
+++ python/main.py
@@ -1,18 +1,34 @@
+import os
 import sys
-sys.path.append('c:/python27/lib/site-packages')
+sys.path.append('/usr/local/lib/python2.7/site-packages')
 import cv2
+import time
+from PIL import Image
+import numpy as np
+
 
 # direction: 'x' or 'y'
 def grad(img, direction):
-    return img
+    kernel = np.zeros((1, 3))
+    kernel[0][0] = -1
+    kernel[0][2] = 1
+    if direction == 'y':
+        kernel = np.zeros((3, 1))
+        kernel[0][0] = -1
+        kernel[2][0] = 1
+
+    dst = cv2.filter2D(img, -1, kernel)
+
+    return dst
 
 def load(path):
     img = cv2.imread(path)
     return img
 
 def main():
-    img = load('Z:/zhoubowei/Pictures/Lenna.png')
+    img = load('/Users/zhoubowei/Pictures/excited.jpeg')
     img_grad = grad(img, 'x')
     cv2.imshow('', img_grad)
+    cv2.waitKey()
 
 main()
